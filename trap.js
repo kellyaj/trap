ee1e8c2158b10e4a5e1963e533fd3018851209de
Andrew Kelly <akelly@8thlight.com>
2014-01-24 14:20:51 -0600

    posting selections to localhost

--- trap.js
+++ trap.js
@@ -1,1 +1,6 @@
-var html = window.getSelection();
+var selection = window.getSelection();
+var url = "http://localhost:5000/create";
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST", url, true);
+xhr.send(selection)
