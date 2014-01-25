0030505849000836239acc51c905d8c5797b1918
Andrew Kelly <akelly@8thlight.com>
2014-01-25 15:01:12 -0600

    regex for fedex number before sending

--- trap.js
+++ trap.js
@@ -1,6 +1,14 @@
 var selection = window.getSelection();
 var url = "http://localhost:5000/create";
 
-var xhr = new XMLHttpRequest();
-xhr.open("POST", url, true);
-xhr.send(selection)
+function isValidFedexNumber(selection) {
+  return /\b((96\d\d\d\d\d ?\d\d\d\d|96\d\d) ?\d\d\d\d ?\d\d\d\d( ?\d\d\d)?)\b/i.test(selection);
+}
+
+if (isValidFedexNumber(selection)) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.send(selection)
+} else {
+  alert('invalid number')
+}
